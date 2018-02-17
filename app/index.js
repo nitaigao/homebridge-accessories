872b4adea4118813b458a95c95c0cdfbb0830da0
Nicholas Kostelnik <nkostelnik@gmail.com>
2018-02-17 15:54:13 +0800

    initial commit

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { Service } from 'homebridge'
+
+
+
+export default GateAccessory
+
+// service.getCharacteristic(Characteristic.TargetDoorState)
+//   .on('get', (callback) => {
+//     var targetState = service.getCharacteristic(Characteristic.TargetDoorState).value;
+//     callback()
+//   })
+//   .on('set', (targetState, callback) => {
+//     //Characteristic.TargetDoorState.OPEN
+//     callback()
+//   });
